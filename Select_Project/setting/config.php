6e5efbd60bfb12d8711ee5c7bb56af6346e913a7
konkanok <konkanoksupharut@gmail.com>
2020-02-29 14:47:29 +0700

    1

---
+++ Select_Project/setting/config.php
@@ -0,0 +1,9 @@
+<?php
+$conn = new mysqli("127.0.0.1","root","","miniproj_select");
+//IP DB server,User,Pass,ชื่อDB
+if($conn->connect_error){
+die("Connection failed:".$conn->connect_error);
+}
+@mysqli_set_charset($conn,"utf8");
+@date_default_timezone_set("asia/bangkok");//ไทม์โซนสำคัญ
+?>
